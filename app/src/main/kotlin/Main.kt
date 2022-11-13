8dd368b4b14b9bcba3d3fc73a49d99b57a1fd44d
Bryce Sanders <104106284+bryce-sanders@users.noreply.github.com>
2022-11-12 21:16:27 -0800

    Length Requirement

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -12,7 +12,9 @@ fun main() {
         println("Please choose one of the following option by entering it's corresponding number:")
         println("   1. Calculate Sphere Volume")
         println("   2. Calculate Cone Volume")
-        println("   3. Quit\n")
+        println("   3. Calculate Cube Volume")
+        println("   4. Calculate Pyramid Volume")
+        println("   5. Quit\n")
 
         /* Read what the user types */
         val choice = readLine()!!
@@ -40,6 +42,30 @@ fun main() {
             println("\nThe cone's volume is about $answer")
         }
         else if (choice.toInt() == 3) {
+            println("\nEnter the length of an edge of the cube:")
+            /* Read what the user types */
+            val edge = readLine()!!
+            /* Get volume of cube with given edge length */
+            val answer = Cube(edge.toDouble()).calculateVolume()
+            /* Tell user the volume of the shape */
+            println("\nThe cube's volume is about $answer")
+        }
+        else if (choice.toInt() == 4) {
+            println("\nEnter the base length of the pyramid:")
+            /* Read what the user types */
+            val baseLen = readLine()!!
+            println("\nEnter the base width of the pyramid:")
+            /* Read what the user types */
+            val baseWid = readLine()!!
+            println("\nEnter the height of the pyramid:")
+            /* Read what the user types */
+            val height = readLine()!!
+            /* Get volume of pyramid with given values */
+            val answer = Pyramid(baseLen.toDouble(), baseWid.toDouble(), height.toDouble()).calculateVolume()
+            /* Tell user the volume of the shape */
+            println("\nThe pyramid's volume is about $answer")
+        }
+        else if (choice.toInt() == 5) {
             /* If the user does not wish to continue, say goodbye and exit the while loop by
             * setting the variable keepRunning's value to false */
             println("\nHave a good day!")
@@ -74,3 +100,28 @@ class Cone(radius:Double, height:Double) {
         return (pi * (radius.pow(2)) * (height/3.0))
     }
 }
+
+// Cube class. Requires the length of an edge.
+class Cube(edge:Double) {
+    /* Property (data member) */
+    private var edge = edge
+
+    /* Member function */
+    fun calculateVolume(): Double {
+        return (edge.pow(3))
+    }
+}
+
+// Pyramid class. Requires base length, base width, and height.
+class Pyramid(baseLen:Double, baseWid:Double, height:Double) {
+    /* Property (data member) */
+    private var baseLen = baseLen
+    private var baseWid = baseWid
+    private var height = height
+
+    /* Member function */
+    fun calculateVolume(): Double {
+        return ((baseLen * baseWid * height)/3.0)
+    }
+}
+
